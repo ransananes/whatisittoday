9d7e9aeb50007321ec62258a144565e8f7691503
ransananes <51711790+ransananes@users.noreply.github.com>
2022-01-14 14:43:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import months from "./data/months.json";
+import parse from "html-react-parser";
 
 function App() {
+  const [data, setData] = useState("");
+  const newDate = new Date();
+
+  useEffect(() => {
+    let day = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    async function fetchWikipediaData() {
+      const url =
+        `http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&titles=` +
+        months[parseInt(month)] +
+        " " +
+        day +
+        `&formatversion=2&rvprop=content&rvslots=*&origin=*`;
+      await axios.get(url).then((res) => {
+        setData(res.data.query.pages[0].extract);
+      });
+    }
+
+    fetchWikipediaData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Wikipedia fetch</h1>
+      {parse(data)}
     </div>
   );
 }
